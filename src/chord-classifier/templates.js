be94cae534d0de20ef6338c472a256135de3bdee
David Mora <davidnmora@gmail.com>
2026-05-25 15:13:36 -0700

    Detect diminished and augmented

--- src/chord-classifier/templates.js
+++ src/chord-classifier/templates.js
@@ -1,4 +1,6 @@
 export const CHORD_TEMPLATES = [
   { suffix: 'major', intervals: [0, 4, 7] },
   { suffix: 'minor', intervals: [0, 3, 7] },
+  { suffix: 'diminished', intervals: [0, 3, 6] },
+  { suffix: 'augmented', intervals: [0, 4, 8] },
 ];
